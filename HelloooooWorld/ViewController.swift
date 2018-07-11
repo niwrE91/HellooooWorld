b72f88f137a56b7bd9174526714e3fb0bf3f661d
Erwin Warkentin <niwrE91@DerGerat-2.fritz.box>
2018-07-11 12:41:18 +0200

    build a little app

--- HelloooooWorld/ViewController.swift
+++ HelloooooWorld/ViewController.swift
@@ -9,9 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var bgImageView: UIImageView!
 
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        logoImageView.isHidden = true
+        bgImageView.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func welcomeButtonTop(_ sender: Any) {
+        logoImageView.isHidden = !true
+        bgImageView.isHidden = !true
+    }
+    
+    @IBAction func byeButtonDown(_ sender: Any) {
+        logoImageView.isHidden = true
+        bgImageView.isHidden = true
+    }
+    
+    
 }
 
